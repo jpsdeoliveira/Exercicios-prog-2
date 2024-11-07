9e98647aaa3532304b8aaaaf68f3ae5b4b0b9edd
João Pedro Oliveira <168736902+jpsdeoliveira@users.noreply.github.com>
2024-11-07 20:16:11 -0300

    Create Solução_4.c

---
+++ TDE 1/Solução_4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define MAX_PRODUTOS 100
+
+typedef struct {
+    int id;
+    char nome[50];
+    float valor;
+} Produto;
+
+float calcularMediaPreco(Produto produtos[], int n) {
+    float soma = 0;
+    for (int i = 0; i < n; i++) {
+        soma += produtos[i].valor;
+    }
+    return soma / n;
+}
+
+int main() {
+    Produto produtos[MAX_PRODUTOS] = {
+        {1, "Arroz", 10.5},
+        {2, "Leite", 20.0},
+        {3, "Biscoito", 30.0},
+        {4, "Bala", 5.5}
+    };
+    int n = 4;
+
+    float media = calcularMediaPreco(produtos, n);
+    printf("A média dos preços dos produtos é: %.2f\n", media);
+
+    return 0;
+}
