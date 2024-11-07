20a3dd58ac4e54dd301ef71fd2d94bd2d456d2b2
João Pedro Oliveira <168736902+jpsdeoliveira@users.noreply.github.com>
2024-11-07 20:41:57 -0300

    Update Solução_1.c

--- TDE 2/Solução_1.c
+++ TDE 2/Solução_1.c
@@ -1,1 +1,18 @@
+/*Exercício 1: Crie um programa que leia uma string e exiba seu conteúdo. Em seguida, exiba o comprimento da string usando a função strlen().*/
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void){
+
+    char str[100];
+
+    printf("Digite uma string: ");
+    scanf("%s", str);
+
+    printf("String digitada: %s\n", str);
+    printf("Comprimento da string: %lu\n", strlen(str));
+    
+    return 0;
+}
