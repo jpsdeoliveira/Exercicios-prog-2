538fe0e6d03447683038c9678e76780853976a36
João Pedro Oliveira <168736902+jpsdeoliveira@users.noreply.github.com>
2024-11-07 20:43:20 -0300

    Update Solução_2.c

--- TDE 2/Solução_2.c
+++ TDE 2/Solução_2.c
@@ -1,1 +1,21 @@
+/*Exercício 2: Implemente um programa que leia duas strings do usuário e verifique se elas são iguais. Exiba uma mensagem informando se as strings são iguais ou diferentes. Use a função strcmp() para a comparação.*/
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+char str1[100], str2[100];
+
+    printf("Digite a primeira string: ");
+    scanf("%s", str1);
+    printf("Digite a segunda string: ");
+    scanf("%s", str2);
+
+    if (strcmp(str1, str2) == 0){
+        printf("As strings são iguais.\n");
+    }else{
+        printf("As strings são diferentes.\n");
+    }
+
+    return 0;
+}
