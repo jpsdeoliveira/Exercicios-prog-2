b7655827ae957bbd31c97c7661cb7317320e9c88
João Pedro Oliveira <168736902+jpsdeoliveira@users.noreply.github.com>
2024-11-07 20:47:25 -0300

    Update Solução_4.c

--- TDE 2/Solução_4.c
+++ TDE 2/Solução_4.c
@@ -1,1 +1,22 @@
+/*Exercício 4: Implemente um programa que leia uma string e verifique quantas vezes o caractere 'a' aparece nela. Exiba o total de ocorrências.*/
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void){
+
+  char str[100];
+  int count = 0;
+
+  printf("Digite uma string: ");
+  scanf("%s", str);
+
+  for (int i = 0; str[i] != '\0'; i++){
+      if (str[i] == 'a'){
+          count++;
+      }
+  }
+
+  return 0;
+}
