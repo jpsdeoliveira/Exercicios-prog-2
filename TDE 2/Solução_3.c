8a0605d37c92d286900d4174fd8dc23de7603072
João Pedro Oliveira <168736902+jpsdeoliveira@users.noreply.github.com>
2024-11-07 20:44:38 -0300

    Update Solução_3.c

--- TDE 2/Solução_3.c
+++ TDE 2/Solução_3.c
@@ -1,1 +1,19 @@
+/*Exercício 3: Escreva um programa que leia uma string do usuário e exiba cada caractere da string em uma linha separada.*/
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void){
+  
+  char str[100];
+
+    printf("Digite uma string: ");
+    scanf("%s", str);
+
+    for (int i = 0; str[i] != '\0'; i++){
+        printf("%c\n", str[i]);
+    }
+
+    return 0;
+}
