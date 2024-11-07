604c117ae560ce69ee33e9e2d6cfb3752f03f289
João Pedro Oliveira <168736902+jpsdeoliveira@users.noreply.github.com>
2024-11-07 20:49:51 -0300

    Update Solução_5.c

--- TDE 2/Solução_5.c
+++ TDE 2/Solução_5.c
@@ -1,1 +1,20 @@
+/*Exercício 5: Crie um programa que leia uma string e a copie para outra variável usando a função strcpy(). Exiba as duas strings para verificar se a cópia foi bem-sucedida.*/
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void){
+
+  char str1[100], str2[100];
+
+  printf("Digite uma string: ");
+  scanf("%s", str1);
+    
+  strcpy(str2, str1);
+
+  printf("String original: %s\n", str1);
+  printf("String copiada: %s\n", str2);
+
+  return 0;
+}
