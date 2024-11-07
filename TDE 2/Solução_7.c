182d3415bbbd81daa7571af194a5772d51d282f6
João Pedro Oliveira <168736902+jpsdeoliveira@users.noreply.github.com>
2024-11-07 20:53:03 -0300

    Update Solução_7.c

--- TDE 2/Solução_7.c
+++ TDE 2/Solução_7.c
@@ -1,1 +1,21 @@
+/*Exercício 7: Escreva um programa que leia uma string e substitua todos os caracteres minúsculos por maiúsculos. Utilize a função toupper() da biblioteca <ctype.h> para auxiliar na conversão.*/
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void){
+
+  char str[100];
+
+  printf("Digite uma string: ");
+  scanf("%s", str);
+
+  for (int i = 0; str[i] != '\0'; i++){
+      str[i] = toupper(str[i]);
+  }
+
+  printf("String em maiúsculas: %s\n", str);
+
+  return 0;
+}
