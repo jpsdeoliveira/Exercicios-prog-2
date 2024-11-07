9149824f21cf58f4240092a66e8edb391eed578f
João Pedro Oliveira <168736902+jpsdeoliveira@users.noreply.github.com>
2024-11-07 20:51:32 -0300

    Update Solução_6.c

--- TDE 2/Solução_6.c
+++ TDE 2/Solução_6.c
@@ -1,1 +1,22 @@
+/*Exercício 6: Implemente um programa que leia duas strings e as concatene em uma nova string. Use a função strcat() para realizar a concatenação e exiba o resultado.*/
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void){
+
+  char str1[100], str2[100], str3[200];
+
+  printf("Digite a primeira string: ");
+  scanf("%s", str1);
+  printf("Digite a segunda string: ");
+  scanf("%s", str2);
+
+  strcpy(str3, str1);
+  strcat(str3, str2);
+
+  printf("String concatenada: %s\n", str3);
+
+  return 0;
+}
